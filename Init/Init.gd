3415a0e7bb3ed01d698e18d467155d2bd4b064b5
WillametteKim <willamettehd@gmail.com>
2019-11-14 16:09:19 +0900

    RESOLVE google play api

--- Init/Init.gd
+++ Init/Init.gd
@@ -1,5 +1,5 @@
 extends Control
-var gpgs = null
+var google = null
 
 func init():
 	# call CheckNetwork()
@@ -9,25 +9,14 @@ func _on_HTTPCheckNetwork_connection_success():
 	#단말이 NW 연결 됐으면 > 로그인 씬으로
 	print('before gpgs')
 	#OS.shell_open("http://localhost:5000")
-	
-#	if Engine.has_singleton("GooglePlay"):
-#		print('init gpgs')
-#		var gpgs = Engine.get_singleton("GooglePlay");
-#		gpgs.init(get_instance_id(), true)
-#		gpgs.keepScreenOn(true)
-#		gpgs.clearCache()
-	#var google = Engine.get_singleton("GooglePlay");
-	#google.init(get_instance_id(), true)
-	
 	Global.goto_scene("res://Init/LoginScene.tscn")
-	
 
 func _on_HTTPCheckNetwork_error_connection_failed(code, message):
 	#단말이 NW 연결 됐으면 > 에러 
 	print('Please check your Network Connection...')
 
-#TODO: 네트워크 연결 확인 
-func _connect():
-	var network = NetworkedMultiplayerENet.new()
-	network.create_client("127.0.0.1", 4242)
-	get_tree().set_network_peer(network)
+##TODO: 네트워크 연결 확인 
+#func _connect():
+#	var network = NetworkedMultiplayerENet.new()
+#	network.create_client("127.0.0.1", 4242)
+#	get_tree().set_network_peer(network)
